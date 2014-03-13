b1a2bd5b2e2be663679ab2086cea16f1ba7da9ac
Clay Stewart <claytonmstewart@gmail.com>
2014-03-13 17:22:46 -0500

    Remove comments from todo item bb model

--- app/assets/javascripts/app/models/todo.js
+++ app/assets/javascripts/app/models/todo.js
@@ -17,21 +17,4 @@
       this.set({description: newTodoDesc}).save();
     }
   });
-  // window.todoItem = new TodoItem({});
 })();
-  // TO LISTEN FOR AN EVENT ON A MODEL
-  // todoItem.on('event-name', function() {
-  //   alert('event-name happened!');
-  // });
-  // RUN THE EVENT
-  // todoItem.trigger('event-name');
-
-  // THE CHANGE EVENT
-  // todoItem.on('change', doThing);
-
-  // if you call .set() on your todoItem -- the change event is automatically triggered
-  // todoItem.set({description: 'Get haircut'});
-  // if want to call it without triggering the change event
-  // todoItem.set({description: 'Get haircut'}, {silent: true});
-  // if want to remove the event listener can call
-  // todoItem.off('change', doThing);
