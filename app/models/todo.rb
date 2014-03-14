e05001bb85f6c1be81933ad27f90ee9c5bb76c7c
Clay Stewart <claytonmstewart@gmail.com>
2014-03-13 19:00:53 -0500

    take created_at and updated_at out of json fetch / put

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,12 @@
 class Todo < ActiveRecord::Base
+
+  def as_json(options={})
+    {
+      id: self.id,
+      description: self.description,
+      status: self.status,
+      # created_at: self.created_at,
+      # updated_at: self.updated_at
+    }
+  end
 end
